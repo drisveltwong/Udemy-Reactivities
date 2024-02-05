cec3de2d5634f65d0203cc81cdb611c7b6d893d1
drisveltwong <drisveltwong@gmail.com>
2024-02-05 13:26:54 +0800

    section 7 MobX (#6)
    Co-authored-by: drisveltwong@gmail.com <79496763+YongXiangOsim@users.noreply.github.com>

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,15 +1,12 @@
 import { SyntheticEvent, useState } from "react";
-import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityList() {
+    const { activityStore } = useStore();
+    const { selectActivity, deleteActivity, loading, activityByDate } = activityStore;
 
-export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: props) {
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
@@ -19,7 +16,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
     return (
         <Segment>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activityByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -32,7 +29,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                                 <Button floated="right" content='View' color="blue" onClick={() => selectActivity(activity.id)} />
                                 <Button
                                     name={activity.id}
-                                    loading={submitting && target == activity.id}
+                                    loading={loading && target == activity.id}
                                     floated="right"
                                     content='Delete'
                                     color="red"
@@ -45,4 +42,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
             </Item.Group>
         </Segment>
     )
-}
+})
