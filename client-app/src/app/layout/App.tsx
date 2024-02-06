3e5a31fe02c3c9a1a9af34bdbdf7b876da19301a
drisveltwong <drisveltwong@gmail.com>
2024-02-06 14:20:47 +0800

    Section 8 router (#7)
    * add react routing
    
    * end of section 8 routing
    
    ---------
    
    Co-authored-by: drisveltwong@gmail.com <79496763+YongXiangOsim@users.noreply.github.com>

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,32 +1,22 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet, useLocation } from 'react-router-dom';
+import { HomePage } from '../../features/home/HomePage';
 
 function App() {
-  const { activityStore: activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-  // [] is location to put dependencies
-  // empty mean that the useEffect if only execute one
-  // when there is dependencies and the dependencies change, the use Effect will execute again
-
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  const location = useLocation();
 
   return (
     <>
-      <NavBar />
-
-      <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
-      </Container>
-
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: '7em' }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </>
 
   )
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,13 +1,24 @@
 import { Grid, List } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
-    const { selectedActivity, editMode } = activityStore;
+    const { loadActivities, activityRegistry } = activityStore;
+
+    useEffect(() => {
+        if (activityRegistry.size <= 1) loadActivities();
+    }, [loadActivities, activityRegistry.size])
+    // [] is location to put dependencies
+    // empty mean that the useEffect if only execute one
+    // when there is dependencies and the dependencies change, the use Effect will execute again
+
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -16,10 +27,7 @@ export default observer(function ActivityDashboard() {
                 </List>
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                    <ActivityDetails />}
-                {editMode &&
-                    <ActivityForm />}
+                <h2>Activity Filters</h2>
             </Grid.Column>
         </Grid>
     )
